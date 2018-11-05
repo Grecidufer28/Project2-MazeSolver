08a10b7d915300d4988711fa0f4dba342671b712
Janice Lu <jjlu555@gmail.com>
2018-11-04 16:31:09 -0800

    Add initial files for project
    includes tester file, node (vertex), and maze file

---
+++ Maze.java
@@ -0,0 +1,14 @@
+package Maze;
+
+import java.util.Stack;
+
+public class Maze {
+	
+	/**
+	 * Creates a perfect maze using Depth First Search algorithm
+	 */
+	void generateMaze() {
+		Stack<Node> cellStack = new Stack<Node>();
+	}
+
+}
---
+++ MazeTester.java
@@ -0,0 +1,54 @@
+package Maze;
+
+/**
+ * Used to test the maze generator and solver for the 
+ * program
+ * @author Anudeep Gogineni
+ * @author Janice Lu
+ *
+ */
+public class MazeTester {
+	
+	//tester for 4x4 maze
+	Maze maze4 = new Maze(4);
+	maze4.generateMaze();
+	maze4.printMaze();
+	maze4.printBFS();
+	maze4.printDFS();
+	
+	//tester for 5x5 maze
+	Maze maze5 = new Maze(4);
+	maze5.generateMaze();
+	maze5.printMaze();
+	maze5.printBFS();
+	maze5.printDFS();
+	
+	//tester for 6x6 maze
+	Maze maze6 = new Maze(4);
+	maze6.generateMaze();
+	maze6.printMaze();
+	maze6.printBFS();
+	maze6.printDFS();
+	
+	//tester for 7x7 maze
+	Maze maze7 = new Maze(4);
+	maze7.generateMaze();
+	maze7.printMaze();
+	maze7.printBFS();
+	maze7.printDFS();
+
+	//tester for 8x8 maze
+	Maze maze8 = new Maze(4);
+	maze8.generateMaze();
+	maze8.printMaze();
+	maze8.printBFS();
+	maze8.printDFS();
+	
+	//tester for 10x10 maze
+	Maze maze10 = new Maze(4);
+	maze10.generateMaze();
+	maze10.printMaze();
+	maze10.printBFS();
+	maze10.printDFS();
+
+}
